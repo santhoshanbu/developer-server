8ab478f36ad723b959b8a1e4cfa64ffe3c39b384
santhoshanbu <asanthosh421@gmail.com>
2023-07-23 15:21:46 +0530

    end point changed

--- serviceEndPoints.js
+++ serviceEndPoints.js
@@ -1,3 +1,3 @@
 import { GraphQLClient } from "graphql-request";
-const client = new GraphQLClient("https://developers.hasura.app/v1/graphql" ,{headers: {"x-hasura-admin-secret": "fXYPjcmYB0wsp5PchwowLsF1fPOKs88rrXiRAJfrs4SS5fqyiyGu02NwYtJMLoWD","hasura-client-name":'server'}})
+const client = new GraphQLClient("https://driven-mink-70.hasura.app/v1/graphql" ,{headers: {"x-hasura-admin-secret": "BdIzCPweHZtR8qAEd5tlwuP4zHs1r6QfClnCEA8vlsMcvoC786QEQ12Rax7tKOsT","hasura-client-name":'server'}})
 export {client} 
